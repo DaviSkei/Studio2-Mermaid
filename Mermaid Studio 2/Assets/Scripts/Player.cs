5eec71de4dbb6330417921ff7f5f8421f9ee53d2
DaviSkei <david.skeie@hotmail.com>
2023-01-20 14:14:37 +0100

    Small changes to player movementspeed

--- Mermaid Studio 2/Assets/Scripts/Player.cs
+++ Mermaid Studio 2/Assets/Scripts/Player.cs
@@ -11,11 +11,14 @@ public class Player : MonoBehaviour
 
     [SerializeField] InventoryObject inventory;
 
+    [SerializeField] float defaultSpeed = 1f;
+    float startSpeed;
+    
     float moveSpeedXZ = 3f;
 
     float moveSpeedY = 2f;
 
-    float rotationTime = 0.1f;
+    float rotationTime = 0.25f;
 
     float rotationSpeed;
 
@@ -26,6 +29,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        startSpeed = defaultSpeed;
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         controller = GetComponent<CharacterController>();
@@ -48,27 +52,6 @@ public class Player : MonoBehaviour
 
         RaycastManager();
 
-        /* bool seeTutorial = Input.GetKeyDown(KeyCode.Tab);
-        bool turnTutorialOff = Input.GetKeyUp(KeyCode.Tab);
-        bool tutorialOn = false;
-        float timer = 0f;
-        if (!tutorialOn && seeTutorial)
-        {
-            Tab4Keybinds.SetActive(false);
-            ControlMenu.SetActive(true);
-            tutorialOn = true;
-            timer += Time.deltaTime;
-        }
-        if (tutorialOn && turnTutorialOff && timer > 1f)
-        {
-            Tab4Keybinds.SetActive(true);
-            ControlMenu.SetActive(false);
-            tutorialOn = false;
-            timer = timer - Time.deltaTime;
-            timer = 0f;
-        }
-        */
-
 
     }
 
@@ -79,12 +62,16 @@ public class Player : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
         float forwardInput = Input.GetAxisRaw("Forward");
 
-        // movement direction vector uses the inputs to determine the new X, Y and Z positions
-        Vector3 movementXZ = new Vector3(horizontal, vertical, forwardInput).normalized;
+        // movement direction vector uses the inputs to determine the new X and Z positions
+        // if i add in the Y component, it moves forward while also moving on Y
+        Vector3 movementXZ = new Vector3(horizontal, 0f, forwardInput).normalized;
 
         Vector3 movementY = new Vector3(0f, vertical, 0f);
 
-        // if movement on x or z axis is not nothing
+        // movementXZ += mainCam.TransformDirection(movementY);
+
+
+        // if movement does not equal zero, that means we are recieving input to move
         if (movementY != Vector3.zero)
         {
             // tell the controller move function to return the vector 3 value of the Y input
@@ -95,17 +82,29 @@ public class Player : MonoBehaviour
         if (movementXZ != Vector3.zero)
         {
 
-            // ability to shift movement direction based on camera direction
+            // ability to rotate the players movement direction
             float targetAngle = Mathf.Atan2(movementXZ.x, movementXZ.z)
             * Mathf.Rad2Deg + mainCam.eulerAngles.y;
 
+            // smooths the rotation movement on the player between its current rotation, to its intended rotation
+            // which is based on the movement inputs
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref rotationSpeed, rotationTime);
 
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
-            
 
-            Vector3 camDirection = Quaternion.Euler(0f, targetAngle, targetAngle) * Vector3.forward;
-            controller.Move(camDirection.normalized * moveSpeedXZ * Time.deltaTime);
+            Vector3 camDirection = Quaternion.Euler(mainCam.rotation.x, targetAngle, 0f) * Vector3.forward;
+            controller.Move(camDirection.normalized * defaultSpeed * Time.deltaTime);
+
+            defaultSpeed += moveSpeedXZ * Time.deltaTime;
+
+            if (defaultSpeed > moveSpeedXZ)
+            {
+                defaultSpeed = moveSpeedXZ;
+            }
+        }
+        else if ( movementXZ == Vector3.zero)
+        {
+            defaultSpeed = startSpeed;
         }
     }
 
@@ -125,7 +124,7 @@ public class Player : MonoBehaviour
     }
     // private void NormalizeSpeed(float speed)
     // {
-    //     moveSpeedXZ = startMoveSpeedXZ;
+    //     moveSpeedXZ = defaultSpeedXZ;
     // }
     void RaycastManager()
     {
