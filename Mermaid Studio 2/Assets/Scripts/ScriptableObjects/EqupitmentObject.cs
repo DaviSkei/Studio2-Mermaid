556071074c058c7435aaeb5c52da229dcf162084
DaviSkei <david.skeie@hotmail.com>
2023-02-23 11:53:58 +0100

    Add Texmesh pro libraries

--- Mermaid Studio 2/Assets/Scripts/ScriptableObjects/EqupitmentObject.cs
+++ Mermaid Studio 2/Assets/Scripts/ScriptableObjects/EqupitmentObject.cs
@@ -5,15 +5,11 @@ using UnityEngine;
 
 public class EqupitmentObject : ItemObject
 {
-    [SerializeField] float durability;
+   
 
     public void Awake()
     {
         // since equpitment items differ from default trash items, we make a new script spesifically for it
         itemType = ItemType.Equpitment;
-    }
-    public float Durability()
-    {
-        return durability;
     }
 }
