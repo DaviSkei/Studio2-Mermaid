74a61b809979df2e6dd6a0146bccd21b8569f5ac
DaviSkei <david.skeie@hotmail.com>
2023-03-05 20:32:46 +0100

    Made some more prefab variants of the jellyfish

--- Mermaid Studio 2/Assets/Scripts/Boids/FishBoidObstacleAvoidance.cs
+++ Mermaid Studio 2/Assets/Scripts/Boids/FishBoidObstacleAvoidance.cs
@@ -11,12 +11,21 @@ public class FishBoidObstacleAvoidance : MonoBehaviour
     private FishBoid boids;
     private Transform boid;
 
+    private JellyfishBoid jellyBoid;
+
     [SerializeField] LayerMask avoidMask;
 
     private void Start()
     {
         boid = transform.parent;
-        boids = boid.GetComponent<FishBoid>();
+        if (boid.GetComponent<FishBoid>() != null)
+        {
+            boids = boid.GetComponent<FishBoid>();
+        }
+        if(boid.GetComponent<JellyfishBoid>() != null)
+        {
+            jellyBoid = boid.GetComponent<JellyfishBoid>();
+        }
     }
 
 
@@ -25,53 +34,113 @@ public class FishBoidObstacleAvoidance : MonoBehaviour
         bool didHit = false;
         RaycastHit hit;
         // Does the ray intersect any objects in the layer mask
-        if (Physics.Raycast(boid.position, boid.forward, out hit, 10, avoidMask))
+        if (boids)
         {
-            Debug.DrawRay(boid.position, boid.forward * hit.distance, Color.red);
-
-            boids.accumAvoid(hit.point);
-
-            didHit = true;
+            if (Physics.Raycast(boid.position, boid.forward, out hit, 10, avoidMask))
+            {
+                Debug.DrawRay(boid.position, boid.forward * hit.distance, Color.red);
+    
+                boids.accumAvoid(hit.point);
+    
+                didHit = true;
+            }
+            if (Physics.Raycast(boid.position, -boid.up, out hit, 10, avoidMask))
+            {
+                Debug.DrawRay(boid.position, -boid.up * hit.distance, Color.red);
+    
+                boids.accumAvoid(hit.point);
+    
+                didHit = true;
+            }
+            if (Physics.Raycast(boid.position, boid.up, out hit, 10, avoidMask))
+            {
+                Debug.DrawRay(boid.position, boid.up * hit.distance, Color.red);
+    
+                boids.accumAvoid(hit.point);
+    
+                didHit = true;
+            }
+            if (Physics.Raycast(boid.position, boid.right, out hit, 10, avoidMask))
+            {
+                Debug.DrawRay(boid.position, boid.right * hit.distance, Color.red);
+    
+                boids.accumAvoid(hit.point);
+    
+                didHit = true;
+            }
+            if (Physics.Raycast(boid.position, -boid.right, out hit, 10, avoidMask))
+            {
+                Debug.DrawRay(boid.position, -boid.right * hit.distance, Color.red);
+    
+                boids.accumAvoid(hit.point);
+    
+                didHit = true;
+            }
+    
+            if (!didHit)
+            {
+                boids.resetAvoid();
+            }
         }
-        if (Physics.Raycast(boid.position, -boid.up, out hit, 10, avoidMask))
+        if (jellyBoid)
         {
-            Debug.DrawRay(boid.position, -boid.up * hit.distance, Color.red);
-
-            boids.accumAvoid(hit.point);
-
-            didHit = true;
+            if (Physics.Raycast(boid.position, boid.forward, out hit, 10, avoidMask))
+            {
+                Debug.DrawRay(boid.position, boid.forward * hit.distance, Color.red);
+    
+                jellyBoid.accumAvoid(hit.point);
+    
+                didHit = true;
+            }
+            if (Physics.Raycast(boid.position, -boid.up, out hit, 10, avoidMask))
+            {
+                Debug.DrawRay(boid.position, -boid.up * hit.distance, Color.red);
+    
+                jellyBoid.accumAvoid(hit.point);
+    
+                didHit = true;
+            }
+            if (Physics.Raycast(boid.position, boid.up, out hit, 10, avoidMask))
+            {
+                Debug.DrawRay(boid.position, boid.up * hit.distance, Color.red);
+    
+                jellyBoid.accumAvoid(hit.point);
+    
+                didHit = true;
+            }
+            if (Physics.Raycast(boid.position, boid.right, out hit, 10, avoidMask))
+            {
+                Debug.DrawRay(boid.position, boid.right * hit.distance, Color.red);
+    
+                jellyBoid.accumAvoid(hit.point);
+    
+                didHit = true;
+            }
+            if (Physics.Raycast(boid.position, -boid.right, out hit, 10, avoidMask))
+            {
+                Debug.DrawRay(boid.position, -boid.right * hit.distance, Color.red);
+    
+                jellyBoid.accumAvoid(hit.point);
+    
+                didHit = true;
+            }
+    
+            if (!didHit)
+            {
+               jellyBoid.resetAvoid();
+            }
         }
-        if (Physics.Raycast(boid.position, boid.up, out hit, 10, avoidMask))
-        {
-            Debug.DrawRay(boid.position, boid.up * hit.distance, Color.red);
-
-            boids.accumAvoid(hit.point);
-
-            didHit = true;
-        }
-        if (Physics.Raycast(boid.position, boid.right, out hit, 10, avoidMask))
-        {
-            Debug.DrawRay(boid.position, boid.right * hit.distance, Color.red);
-
-            boids.accumAvoid(hit.point);
-
-            didHit = true;
-        }
-        if (Physics.Raycast(boid.position, -boid.right, out hit, 10, avoidMask))
-        {
-            Debug.DrawRay(boid.position, -boid.right * hit.distance, Color.red);
-
-            boids.accumAvoid(hit.point);
-
-            didHit = true;
-        }
-
-        if (!didHit)
-            boids.resetAvoid();
     }
     private void OnTriggerExit(Collider other)
     {
-        boids.resetAvoid();
+        if (boid.GetComponent<FishBoid>() != null)
+        {
+            boids.resetAvoid();
+        }
+        if(boid.GetComponent<JellyfishBoid>() != null)
+        {
+            jellyBoid.resetAvoid();
+        }
     }
 }
 
