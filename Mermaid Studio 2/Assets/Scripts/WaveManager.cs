638f1c5975392aedd2888f3c66d3f014b4a2068d
DaviSkei <david.skeie@hotmail.com>
2023-03-10 20:13:18 +0100

    I hate bouyancy

--- Mermaid Studio 2/Assets/Scripts/WaveManager.cs
+++ Mermaid Studio 2/Assets/Scripts/WaveManager.cs
@@ -4,22 +4,46 @@ using UnityEngine;
 
 public class WaveManager : MonoBehaviour
 {
-    private float waveHeight = 0.0008f, waveSpeed = 0.05f, waveFrequency = 6f;
+    [SerializeField] private float waveHeight = 0.0008f, waveSpeed = 0.05f, waveFrequency = 6f;
+    // freq = ripple scale, speed = ripple speed, height = waveheight
 
     Material material;
     Texture2D displacementTexture;
 
     void Start()
     {
+        SetVariables();
+    }
+    void SetVariables()
+    {
         material = GetComponent<MeshRenderer>().material;
         displacementTexture = (Texture2D)material.GetTexture("_WaveDisplacement");
+
     }
 
     public float WaveHeight(Vector3 pos)
     {
-        return displacementTexture.GetPixelBilinear
-        (pos.x * waveFrequency, pos.z * waveFrequency + Time.deltaTime * (waveSpeed * -1f)).g * 
-        (waveHeight * -0.5f) * transform.localScale.x;
+        return transform.position.y + displacementTexture.GetPixelBilinear
+        (pos.x * waveFrequency, pos.z * waveFrequency + Time.deltaTime * waveSpeed).g * waveHeight * transform.localScale.x;
+
+
+        // (pos.x * waveFrequency, pos.z * waveFrequency + Time.deltaTime * (waveSpeed * -1f)).g * 
+        // (waveHeight * -0.5f) * transform.localScale.x;
     }
-    
+    // void OnValidate()
+    // {
+    //     if (!material)
+    //     {
+    //         SetVariables();
+
+    //         UpdateMaterial();
+    //     }
+    // }
+
+    // void UpdateMaterial()
+    // {
+    //     material.SetFloat("_Ripple_Speed", waveSpeed);
+    //     material.SetFloat("_Riple_Scale", waveFrequency);
+    //     material.SetFloat("_WaveHeight", waveHeight);
+    // } 
 }
