bc2898b4dddb57eaf283e3c8ffe67a561560bae2
DaviSkei <114075714+DaviSkei@users.noreply.github.com>
2023-04-06 20:00:18 +0200

    Done with boids

--- Mermaid Studio 2/Assets/Scripts/SpiralSwarm.cs
+++ Mermaid Studio 2/Assets/Scripts/SpiralSwarm.cs
@@ -6,10 +6,16 @@ public class SpiralSwarm : MonoBehaviour
 {
     float moveSpeed, rotateSpeed;
 
+    [Range(1, 30)]
+    [SerializeField] float minMoveSpeed = 8, maxMoveSpeed = 12;
+    [Range(1, 30)]
+    [SerializeField] float minRotSpeed = 8, maxRotSpeed = 12;
+
+
     void Start()
     {
-        moveSpeed = Random.Range(8, 12);
-        rotateSpeed = Random.Range(80, 120);
+        moveSpeed = Random.Range(minMoveSpeed, maxMoveSpeed);
+        rotateSpeed = Random.Range(minRotSpeed, maxRotSpeed);
     }
     void Update()
     {
