557368cdbb90f260af4dea96dc114c3624a4205c
DaviSkei <david.skeie@hotmail.com>
2023-04-07 16:24:39 +0200

    Net animation working

--- Mermaid Studio 2/Assets/Scripts/Turtle.cs
+++ Mermaid Studio 2/Assets/Scripts/Turtle.cs
@@ -9,18 +9,15 @@ public class Turtle : MonoBehaviour
     [SerializeField] private float rotateSpeed = 2f;
 
     [SerializeField] private bool isTrapped = false;
-
-    Vector3 avoidObst;
-
-    Vector3 velocity;
-    int avoidCount = 0;
+    
+    GameObject netObj;
 
     private string inNet = "inNet";
     // Start is called before the first frame update
     void Start()
     {
         turtleAnimator = GetComponent<Animator>();
-        velocity = new Vector3(0,0,0);
+        netObj = GetComponentInChildren<FishNet>().gameObject;
     }
 
     // Update is called once per frame
@@ -32,57 +29,20 @@ public class Turtle : MonoBehaviour
         }
         else
         {
-            MoveLogic();
             Move();
         }
     }
     private void TrappedLogic()
     {
         turtleAnimator.SetBool(inNet, true);
+        netObj.SetActive(true);
     }
-    // private void Movement()
-    // {
-    //     isTrapped = false;
-    //     transform.position += transform.forward * moveSpeed * Time.deltaTime;
-    //     transform.Rotate(Vector3.up * (rotateSpeed * Time.deltaTime));
-    // }
-    private void MoveLogic()
-    {
-        Ray ray = new Ray(transform.position, transform.forward);
-        RaycastHit hit;
-        float rayDist = 10f;
+    void Move()
+    { 
+        transform.position += transform.forward * moveSpeed * Time.deltaTime;
+        transform.Rotate(Vector3.up * (rotateSpeed * Time.deltaTime));
+        turtleAnimator.SetBool(inNet, false);
+        netObj.SetActive(false);
+    }
 
-        if (Physics.Raycast(ray, out hit, rayDist))
-        {
-            AccumAvoid(hit.point);
-            Move();
-        }
-    }
-    private void AccumAvoid(Vector3 avoid)
-    {
-        avoidObst += transform.position - avoid;
-        avoidCount++;
-    }
-    private Vector3 Avoid()
-    {
-
-        if (avoidCount > 0)
-        {
-            return (avoidObst / avoidCount).normalized ;
-        }
-
-        return Vector3.zero;
-    }
-    private void Move()
-    {
-        Vector3 newVelocity = new Vector3(0,0,0);
-        newVelocity += Avoid();
-        Vector3 slerpVelo = Vector3.Slerp(velocity, newVelocity, Time.deltaTime);
-    
-        velocity = slerpVelo.normalized;
-    
-        transform.position += velocity * Time.deltaTime * moveSpeed;
-        transform.LookAt(transform.position + velocity);
-        transform.Rotate(velocity);
-    }
 }
