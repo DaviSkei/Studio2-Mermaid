0c26f3a0a97c15cd45e722cd783f8e0b3c3c4c7f
DaviSkei <114075714+DaviSkei@users.noreply.github.com>
2023-04-09 15:09:49 +0200

    Net cutting mechanic finished

--- Mermaid Studio 2/Assets/Scripts/Turtle.cs
+++ Mermaid Studio 2/Assets/Scripts/Turtle.cs
@@ -10,14 +10,14 @@ public class Turtle : MonoBehaviour
 
     [SerializeField] private bool isTrapped = false;
     
-    GameObject netObj;
+    FishNet net;
 
     private string inNet = "inNet";
     // Start is called before the first frame update
     void Start()
     {
         turtleAnimator = GetComponent<Animator>();
-        netObj = GetComponentInChildren<FishNet>().gameObject;
+        net = GetComponentInChildren<FishNet>();
     }
 
     // Update is called once per frame
@@ -27,22 +27,25 @@ public class Turtle : MonoBehaviour
         {
             TrappedLogic();
         }
-        else
+        if (net.isCut)
         {
-            Move();
+            isTrapped = false;
+            if (!isTrapped)
+            {
+                Move();
+            }
         }
     }
     private void TrappedLogic()
     {
         turtleAnimator.SetBool(inNet, true);
-        netObj.SetActive(true);
+        net.gameObject.SetActive(true);
     }
     void Move()
     { 
         transform.position += transform.forward * moveSpeed * Time.deltaTime;
         transform.Rotate(Vector3.up * (rotateSpeed * Time.deltaTime));
-        turtleAnimator.SetBool(inNet, false);
-        netObj.SetActive(false);
+        turtleAnimator.SetBool(inNet, isTrapped);
     }
 
 }
