9b69d8c97f0f07a9d3a2c89e8f0caeaedaae8f7a
DaviSkei <114075714+DaviSkei@users.noreply.github.com>
2023-04-16 16:48:34 +0200

    Changing some fish settings

--- Mermaid Studio 2/Assets/Scripts/Turtle.cs
+++ Mermaid Studio 2/Assets/Scripts/Turtle.cs
@@ -8,19 +8,29 @@ public class Turtle : MonoBehaviour
     [SerializeField] private float moveSpeed = 0.5f;
     [SerializeField] private float rotateSpeed = 2f;
 
+    float multRotSpeed, slowRotSpeed;
+
     [SerializeField] private bool isTrapped = false;
     
     FishNet net;
 
     private string inNet = "inNet";
+
+    RaycastHit hit;
+    float rayDist = 7f;
+    LayerMask layerMask = 1 << 10;
+
     // Start is called before the first frame update
     void Start()
     {
+        multRotSpeed = rotateSpeed * 6;
+        slowRotSpeed = rotateSpeed * 0.2f;
+
         turtleAnimator = GetComponent<Animator>();
         net = GetComponentInChildren<FishNet>();
         if(isTrapped)
         {
-            net.gameObject.SetActive(true);
+            TrappedLogic();
         }
         else
         {
@@ -51,9 +61,35 @@ public class Turtle : MonoBehaviour
     }
     void Move()
     { 
-        transform.position += transform.forward * moveSpeed * Time.deltaTime;
-        transform.Rotate(Vector3.up * (rotateSpeed * Time.deltaTime));
         turtleAnimator.SetBool(inNet, isTrapped);
+        if (RayManager())
+        {
+            MoveForward();
+            transform.Rotate((Vector3.up + (-Vector3.right)) * (multRotSpeed * Time.deltaTime));
+        }
+        else
+        {
+            MoveForward();
+            
+            transform.Rotate(Vector3.up * (rotateSpeed * Time.deltaTime));
+            transform.Rotate(Vector3.right * (slowRotSpeed * Time.deltaTime));
+        }
+    }
+    void MoveForward()
+    {
+        transform.position += transform.forward * moveSpeed * Time.deltaTime;
+    }
+    bool RayManager()
+    {
+        // down ray
+        if (Physics.Raycast(transform.position, -transform.up, out hit, rayDist, layerMask.value))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
 }
