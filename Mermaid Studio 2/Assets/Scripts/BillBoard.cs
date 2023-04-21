fae52cf8543b6b249e8cc4b4e9cfed83c4ac9a4b
DaviSkei <114075714+DaviSkei@users.noreply.github.com>
2023-04-21 19:48:27 +0200

    Godrays Finished

---
+++ Mermaid Studio 2/Assets/Scripts/BillBoard.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillBoard : MonoBehaviour
+{
+    void LateUpdate()
+    {
+        transform.LookAt(Camera.main.transform.position, Vector3.up);
+    }
+}
